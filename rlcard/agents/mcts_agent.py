918491ad65eaba2628a63ac7e91319b59c9f97b3
clairemoonmatthews-ui <clairemoonmatthews@gmail.com>
2026-02-06 03:48:02 +0000

    222send to cloud

--- rlcard/agents/mcts_agent.py
+++ rlcard/agents/mcts_agent.py
@@ -7,6 +7,7 @@ from rlcard.agents.transformer_agent import TransformerAgent
 
 
 class Node:
+    """Simple class to represent nodes in an MCTS tree."""
     def __init__(self, num_actions:int, num_players:int, parent:Optional['Node']=None):
         self.parent = parent
         self.children = {}
@@ -14,17 +15,16 @@ class Node:
         self.values = np.zeros([num_actions, num_players], dtype=np.float32)
 
     def best_action(self) -> int:
+        """Selects the best action using visit counts."""
         return np.argmax(self.visits)
 
 class MCTSAgent:
+    """Agent using MCTS and another agent"""
     def __init__(self, agent:TransformerAgent, game_class:Type, num_simulations:int=100, c_puct=sqrt(2)):
-
         self.agent = agent
         self.game_class = game_class
         self.num_simulations = num_simulations
         self.c_puct = c_puct
-
-    # def pick_move(self, game, ):
 
     def step(self, state:Dict) -> int:
         """
@@ -44,4 +44,40 @@ class MCTSAgent:
         return root.best_action()
 
     def simulate(root:Node, obs:np.array):
+        """
+        Run one step of simulation of MCTS.
+
+        Arguments:
+            root: Node we're starting with
+            obs: Observations from game        
+        """
         pass
+
+        # We are implementing Alpha Zero style MCTS with imperfect information.
+        # We need to do the following five steps:
+        # 1. Determinize: Create a realized game state from the observations.
+        # 2. Selection: Starting at the root, use PUCT to select actions until
+        #    either you reach an unexpanded node, or you reach a terminal state.
+        #    We get the policy prior from the transformer agent.
+        # 3. Expansion: Unless you are at a terminal state, create one new node.
+        # 4. Evaluate: At terminal states, find the actual payoffs; 
+        #    otherwise use the transformer agent to estimate value.
+        # 5. Backpropagation: Adjust all the nodes back to the root with visit count 
+        #    and updated Q-value.
+
+        # Hmm.  Might want to change this method to take an rlcard state dict 
+        # instead of obs.
+
+        # Some useful methods:
+        # self.agent.get_policy() - not yet implemented
+        # self.agent.get_value()
+        # self.game_class.from_rlcard_state() 
+        # game.is_over()
+        # game.step()
+        # game.get_rlcard_state() - returns both obs and legal_actions
+        # game.get_payoffs() - not yet implemented, but see SergeantMajorEnv; think about scaling 
+        # game.get_player_id()
+
+        # PUCT = values_for_current_player + c_puct * priors * sqrt(total_visits + 1) / (1 + visits)
+        # Note that values, priorts and visits should be indexed by legal actions.
+
